c87375ecfe58bf4d1fec31377ce0c3f33c52e2d1
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2025-02-05 11:00:10 +0100

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# Viam TypeScript SDK MQL/SQL Querying Quickstart
+
+This example demonstrates how to connect to the Viam app and make calls to it.
+
+## Prerequisits
+
+Install [nvm](https://github.com/nvm-sh/nvm?tab=readme-ov-file#installing-and-updating)
+
+Then install node/npm:
+
+```
+nvm use 22
+```
+
+## Setup
+
+From within the project folder run `npm install`
+
+## Usage
+
+Run `npm run dev` and visit `localhost:5173` in a browser. Press the button to execute the logic defined in `src/main.ts`.
+
+Edit `src/main.ts` to change the logic being run. Edit `index.html` to change the layout of the app.
---
+++ src/main.ts
@@ -0,0 +1,81 @@
+import * as VIAM from "@viamrobotics/sdk";
+import { BSON } from "bsonfy";
+
+const ORG_ID = import.meta.env.VITE_ORG_ID;
+const API_KEY_ID = import.meta.env.VITE_API_KEY_ID;
+const API_KEY_SECRET = import.meta.env.VITE_API_KEY_SECRET;
+
+async function connect(): Promise<VIAM.ViamClient> {
+  const opts: VIAM.ViamClientOptions = {
+    credentials: {
+      type: "api-key",
+      authEntity: API_KEY_ID,
+      payload: API_KEY_SECRET,
+    },
+  };
+
+  const client = await VIAM.createViamClient(opts);
+
+  return client;
+}
+
+const button = <HTMLButtonElement>document.getElementById("main-button");
+
+async function run(client: VIAM.ViamClient) {
+  try {
+    button.disabled = true;
+    const textElement = <HTMLParagraphElement>document.getElementById("text");
+    textElement.innerHTML = "waiting for data...";
+
+    // SQL Query
+    /*
+    const dataList = await client.dataClient.tabularDataBySQL(
+      ORG_ID,
+      "select * from readings limit 5"
+    );
+    */
+
+    // MQL Query
+    const jsonQuery = [
+      {
+        $match: {
+          organization_id: "96b696a0-51b9-403b-ae0d-63753923652f",
+        },
+      },
+      {
+        $limit: 5,
+      },
+    ];
+    const bsonQuery = jsonQuery.map((stage) => BSON.serialize(stage));
+    const dataList = await client.dataClient.tabularDataByMQL(
+      ORG_ID,
+      bsonQuery
+    );
+
+    // Display the data
+    textElement.innerHTML = JSON.stringify(dataList, null, 2);
+  } finally {
+    button.disabled = false;
+  }
+}
+
+async function main() {
+  let client: VIAM.ViamClient;
+  try {
+    button.textContent = "Connecting...";
+    client = await connect();
+    button.textContent = "Click for data";
+  } catch (error) {
+    button.textContent = "Unable to connect";
+    console.error(error);
+    return;
+  }
+
+  // Make the button in our app do something interesting
+  button.addEventListener("click", async () => {
+    await run(client);
+  });
+  button.disabled = false;
+}
+
+main();
